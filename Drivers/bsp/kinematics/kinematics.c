ab4a9ed0318602f84565956dd0b820ef93ac3d47
良木同学 <15368311317@163.com>
2024-04-09 09:49:09 +0800

    添加dsp数学库

--- Drivers/bsp/kinematics/kinematics.c
+++ Drivers/bsp/kinematics/kinematics.c
@@ -1,3 +1,11 @@
 //
 // Created by 15368 on 2024/4/9.
 //
+
+#include "kinematics.h"
+
+
+
+
+
+
--- Drivers/bsp/kinematics/kinematics.h
+++ Drivers/bsp/kinematics/kinematics.h
@@ -2,7 +2,19 @@
 // Created by 15368 on 2024/4/9.
 //
 
-#ifndef V2_KINEMATICS_H
-#define V2_KINEMATICS_H
+#ifndef __KINEMATICS_H
+#define __KINEMATICS_H
 
-#endif //V2_KINEMATICS_H
+
+#include "main.h"
+#include "math.h"
+
+
+
+
+
+
+
+
+
+#endif //__KINEMATICS_H
